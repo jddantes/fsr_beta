7756562823454c5f8b0fb230b493e6e04b46b989
Jd Dantes <jddantes@gmail.com>
2014-01-02 17:49:16 +0800

    Added navigation to account.php

--- account.php
+++ account.php
@@ -5,6 +5,8 @@ include('import.php');
 ?>
 <html>
 <h1>Change Account Settings</h1>
+<a href="home.php">Home</a><br/>
+<a href="profile.php">Edit Profile</a><br/>
 Change Password</br>
 <form action="accountsave.php" method="post">
 Old Password: <input type="password" name="input_old_password"/><br/>
